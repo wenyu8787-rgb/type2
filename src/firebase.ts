d149e05f4bcc16c8b3d3e3466a7ed89a52082032
wenyu8787-rgb <wenyu8787@gmail.com>
2026-04-16 11:36:59 +0800

    更新 src/firebase.ts via EZPage

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// @ts-ignore
+import firebaseConfig from '../firebase-applet-config.json';
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+
+export const login = () => signInWithPopup(auth, provider);
